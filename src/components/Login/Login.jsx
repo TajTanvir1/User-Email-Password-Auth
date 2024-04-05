b30d832881b4900d108576b13e1d9cd14ea965de
tajtanvvir1 <tanvirah1223@gmail.com>
2024-04-06 01:54:46 +0600

    Verify added'

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,102 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { useRef, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import auth from "../../firebase/firebase.config";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+   const [showPassword, setShowPassword] = useState(false);
+   const [registerError, setRegisterError] = useState('');
+   const [success, setSuccess] = useState('');
+   const emailRef = useRef(null);
+
+   const handleLogin = e => {
+      e.preventDefault();
+      const email = e.target.email.value;
+      const password = e.target.password.value;
+      console.log(email, password);
+
+      // Reset Error
+      setRegisterError('');
+      setSuccess('')
+
+
+      // Add Validation
+      signInWithEmailAndPassword(auth, email, password)
+         .then(result => {
+            console.log(result.user);
+            if(result.user.emailVerified){
+               setSuccess('User Login Successfully')
+            }
+            else{
+               alert('Please Verify Your Account')
+            }
+         })
+         .catch(error => {
+            console.error(error)
+            setRegisterError(error.message)
+
+         })
+   }
+
+   const handleForgetPassword = () =>{
+      const email = emailRef.current.value;
+      if(!email){
+         console.log('Please Write a Email', emailRef.current.value);
+         return;
+      }
+      else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+         console.log('Please provide a valid email');
+         return;
+      }
+      sendPasswordResetEmail(auth, email)
+      .then(()=>{
+         alert('Please check your email')
+      })
+      .catch(error =>{
+         console.log(error)
+      })
+   }
+
    return (
-      <div>
-         <h2 className="text-3xl">Please Login</h2>
+      <div className="border p-4 rounded-xl">
+         <h2 className="text-3xl ">Login Here</h2>
+         <div>
+            <form onSubmit={handleLogin}>
+               <input 
+               className="mb-4 px-4 py-3 w-1/3 rounded-lg" type="email" 
+               name="email" 
+               ref={emailRef}
+               placeholder="Email Address" 
+               required />
+               <br />
+               <div className="relative">
+                  <input
+                     className="px-4 py-3 my-2 w-1/3 rounded-lg"
+                     type={showPassword ? "text" : "password"}
+                     name="password"
+                     placeholder="Password" required />
+                  <span className="absolute top-1/3 ml-2" onClick={() => setShowPassword(!showPassword)}>
+                     {
+                        showPassword ? <FaEyeSlash /> : <FaEye />
+                     }
+                  </span>
+                  <label className="label">
+                     <a onClick={handleForgetPassword} href="#" className="label-text-alt mx-auto link link-hover">Forgot password?</a>
+                  </label>
+               </div>
+               <br />
+
+               <input className="btn btn-primary w-1/3 mt-3" type="submit" value="Login" />
+            </form>
+            {
+               registerError && <p className="text-red-400">{registerError}</p>
+            }
+            {
+               success && <p className="text-green-500">{success}</p>
+            }
+            <p>Already Have an Account? Please <Link className="text-blue-500 font-bold" to='/register'>Resister</Link></p>
+         </div>
       </div>
    );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,8 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
+import { FaEye, FaEyeSlash  } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Register = () => {
    const [registerError, setRegisterError] = useState('');
@@ -11,12 +13,18 @@ const Register = () => {
 
    const handleRegister = e => {
       e.preventDefault();
+      const name = e.target.name.value;
       const email = e.target.email.value;
       const password = e.target.password.value;
-      console.log(email, password);
+      const accepted = e.target.terms.checked;
+      console.log(email, password, accepted, name);
       // Password Length & Requires
       if(password.length < 6){
          setRegisterError('Password should be at Least 6 Characters or Longer');
+         return;
+      }
+      else if(!accepted){
+         setRegisterError('Please Accept our Terms & Conditions');
          return;
       }
       // else if(!regularExpression.test(password)){
@@ -40,11 +48,31 @@ const Register = () => {
       // Reset Error
       setRegisterError('');
       setSuccess('')
+
       // create user
       createUserWithEmailAndPassword(auth, email, password)
       .then(result=>{
          console.log(result.user),
          setSuccess('User Created Successfully')
+
+         // Update User
+         updateProfile(result, {
+            displayName: name,
+            photoURL: "https://example.com/jane-q-user/profile.jpg"
+         })
+         .then(()=> console.log('Profile Updated'))
+
+         .catch(error => {
+            console.log(error.message)
+         })
+
+         // Send verification Email
+         sendEmailVerification(result.user)
+         .then(() => {
+            alert('Please Check your Email and verify your account')
+         })
+
+
       })
       .catch(error => [
          console.error(error),
@@ -59,14 +87,25 @@ const Register = () => {
          <h2 className="text-3xl ">Please Register</h2>
          <div>
             <form onSubmit={handleRegister}>
+               <input className="mb-4 px-4 py-3 w-1/3 rounded-lg" type="name" name="name" placeholder="Your Name" required />
+               <br />
                <input className="mb-4 px-4 py-3 w-1/3 rounded-lg" type="email" name="email" placeholder="Email Address" required />
                <br />
+               <div className="relative">
                <input 
                className="px-4 py-3 my-2 w-1/3 rounded-lg" 
                type={showPassword ? "text" : "password"}
                name="password" 
                placeholder="Password" required />
-               <span onClick={() => setShowPassword(!showPassword)}>Show</span>
+               <span className="absolute top-1/3 ml-2" onClick={() => setShowPassword(!showPassword)}>
+                  {
+                  showPassword ? <FaEyeSlash /> : <FaEye /> 
+                  }
+                  </span>
+               </div>
+               <br />
+               <input type="checkbox" name="terms" />
+               <label className="ml-2" htmlFor="terms">Accept Our <a href="">Terms & Conditions</a></label>
                <br />
                <input className="btn btn-secondary w-1/3 mt-3" type="submit" value="Register" />
             </form>
@@ -76,6 +115,7 @@ const Register = () => {
             {
                success && <p className="text-green-500">{success}</p>
             }
+            <p>New to this website? Please <Link className="text-blue-500 font-bold" to='/login'>Login</Link></p>
          </div>
 
       </div>
